52b2eb501818df43a6166e1fa7f3171aa4fdbce3
kurtsundell <kurtsundell@gmail.com>
2020-01-30 12:00:47 -0700

    poi

--- DZnmf_v05.m
+++ DZnmf_v05.m
@@ -44,6 +44,14 @@ else
     gui_mainfcn(gui_State, varargin{:});
 end
 
+% set paths for writing spreadsheets
+javaaddpath('poi_library/poi-3.8-20120326.jar');
+javaaddpath('poi_library/poi-ooxml-3.8-20120326.jar');
+javaaddpath('poi_library/poi-ooxml-schemas-3.8-20120326.jar');
+javaaddpath('poi_library/xmlbeans-2.3.0.jar');
+javaaddpath('poi_library/dom4j-1.6.1.jar');
+javaaddpath('poi_library/stax-api-1.0.1.jar');
+
 % End initialization code - DO NOT EDIT
 
 function DZnmf_v05_OpeningFcn(hObject, eventdata, H, varargin)
